7d88250112264221929375f57a4e6b3221157783
Anh NGUYEN HOANG PHUONG <anh.nguyen-hoang-phuong@etu.univ-nantes.fr>
2023-04-09 11:12:43 +0000

    Replace procession.hpp

--- src/procession.hpp
+++ src/procession.hpp
@@ -1,85 +1,46 @@
-/*Procession <=> Cortege*/
+#include "procession.hpp"
 
-#ifndef PROCESSION_HPP
-#define PROCESSION_HPP
+#include <iostream>
+using namespace std;
 
-#include <vector>
-#include <list>
-#include "group.hpp"
+Procession::Procession(const std::string &name){
+    this->name = name; 
+}
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// because We are also interested in the identification and extraction of a person knowing the identifier//
-/// in case of emergency,                                                                                 //
-/// This is the operations involve inserting or deleting elements in the middle of the structure.         //
-/// So I dicided using LINKED LIST as std::list                                                                         //
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class Procession
-{
-private:
-    std::string name;
-    std::list<Group*> groups;
 
-public:
-    Procession(const std::string &name);
-    ~Procession();
-
-    /// @brief insertion of a Group into Procession
-    /// @param group
-    void addGroup(Group *group);
-
-    //// @brief deletion of a Group ////
-    //// @param name of the Group ////
-    void removeGroup(const std::string &name);
-
-    /// @brief acces a person in Procession ////
-    /// @param id identifier of the Person ///
-    /// @return return person
-    Person *getPerson(int id);
-
-    /// @brief deletion person in Procession ////
-    /// @param id identifier of the Person
-    void removePerson(int id);
-
-    /// @brief sorting by color of the groups ///
-    void sortColor();
-
-    /// @brief sorting by Size of the groups ///
-    void sortSize();
-
-     // Iterators: a nested class that represents an iterator for the Procession class
-   class iterator {
-    private:
-        std::list<Group*>::iterator group_it;
-
-    public:
-        iterator(std::list<Group*>::iterator it) : group_it(it) {}
-
-        
-        //Opérateur de comparaison pour tester l'égalité de deux itérateurs
-        bool operator==(const iterator& other) const {
-            return group_it == other.group_it;
-        }
-
-        bool operator!=(const iterator& other) const {
-            return group_it != other.group_it;
-        }
-
-        iterator operator++() {
-            ++group_it;
-            return *this;
-        }
-
-        Group& operator*() const {
-            return *(*group_it);
-        }
-    };
-    // Méthode begin() pour obtenir un itérateur pointant sur le premier groupe du cortège
-    iterator begin() {
-        return iterator(groups.begin());
+void Procession::addGroup(Group *group){
+    if (groups.empty() || group->getName() < groups.front()->getName()) {
+        groups.push_front(group); 
     }
-    // Méthode end() pour obtenir un itérateur pointant après le dernier groupe du cortège
-    iterator end() {
-        return iterator(groups.end());
+    else {
+        std::list<Group*>::iterator it;
+        for (it = groups.begin(); it != groups.end(); it++) {
+            if ((*it)->getName() > group->getName()) {
+                groups.insert(it, group);
+                break;
+            }
+        }
+        if (it == groups.end()) {
+            groups.push_back(group);
+        }
     }
-}; 
-#endif
+}
+
+
+void Procession::removeGroup(const std::string &name){
+    for (const auto &rm : groups){
+        if (rm->getName() == name){
+            
+        }
+    }
+}
+Person Procession::*getPerson(int id){}
+void Procession::removePerson(int id){}
+void Procession::sortColor(){}
+void Procession::sortSize(){}
+Procession::~Procession() {
+        for (auto it = groups.begin(); it != groups.end(); ++it) {
+            delete *it;
+        }
+    }
+   
