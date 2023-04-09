3446ee86b356b24711b260db56ed0940e1060c87
Anh NGUYEN HOANG PHUONG <anh.nguyen-hoang-phuong@etu.univ-nantes.fr>
2023-04-09 11:04:07 +0000

    Replace group.hpp

--- src/group.hpp
+++ src/group.hpp
@@ -9,28 +9,35 @@
 /////////////////////////////////////////////////////////////////////////////////
 // We are interested in the access to a person from the identifier in the group//
 // The time complexity must be a Θ(1). So I use Hashtable here                 //
+//because Group is a collection of people ordered from oldest (first) to newest (last)
+// So I decided 
 
 class Group {
     private:
+     
+     struct Node {
+        Person p;
+        Node* next;
+        Node* prev; 
+        Node() : p(p.getName(), p.getID()), next(nullptr), prev(nullptr) {}
+    };
         std::string name;
         std::string color;
-        Person* leader;
-        Person* last;
-        std::list<Person> peopleQ;
-        std::unordered_map<int, Person*> groupMap;
+        Node* leader;
+        Node* last;
+      
+        //std::list<Person> peopleQ;
+        std::unordered_map<int, Node* > groupMap;
 
     public:
         Group(std::string name, std::string color);
         ~Group();
 
-        int getSize() const; 
-        std::string getName() const; 
-        std::string getColor() const; 
-        Person* getPerson(int id) const;
-        Person* getLeader() const;
-
-        //TODO: discuss if this should be in constructor
-        //std::unordered_map<int, Person*> createGroup();
+        int getSize() const;
+        std::string getName() const;
+        std::string getColor() const;
+        Person getPerson(int id) const;
+        Person getLeader() const;
 
         void insertPerson(Person* person);
         void removePerson(int id);
@@ -50,23 +57,8 @@ class Group {
         };
 
         Iterator begin() const;
-        Iterator end() const;
+        Iterator end() const; 
 }; 
 
-
-/* class Group {
-public:
-    Group();
-    ~Group();
-    int taille() const;
-    std::shared_ptr<Person> acces_person(const std::string& id) const;
-    void insertion_person(const std::string& id);
-    void suppression_person(const std::string& id);
-    void suppression_premiere_person();
-    std::shared_ptr<Person> acces_leader() const;
-private:
-    std::shared_ptr<Person> leader_;
-    std::unordered_map<std::string, std::shared_ptr<Person>> persons_;
-}; */
 
 #endif
